c513d327754f98dc62c9e9fac1d6b4c44fffca61
cshedlick <carly@automatedinsights.com>
2017-01-18 09:48:07 -0500

    Initial Commit

--- currency.rb
+++ currency.rb
@@ -1,6 +1,12 @@
 class Currency
-  def initialize(amount:, code:)
+  attr_reader :amount, :code # note :s at the beginning here
+
+  def initialize(amount:, code:) # when sending in parameters with names do :s after
     @amount = amount
     @code = code
+  end
+
+  def == (other)
+    @amount == other.amount && @code == other.code
   end
 end
--- currency_test.rb
+++ currency_test.rb
@@ -2,8 +2,17 @@ require 'minitest/pride'
 require 'minitest/autorun'
 require './currency.rb'
 
-class CurrencyTest < Minitest::test
-  def test_currency_creation ## need to start with test_ then can call whatever you want
+class CurrencyTest < Minitest::Test
+  def test_currency_creation ## need to start with test_ then can call it whatever you want
     Currency.new(amount: 34, code: 'USD')
+  end
+
+  def test_equal_currencies
+    a = Currency.new(amount: 34, code: 'USD')
+    b = Currency.new(amount: 34, code: 'USD')
+    assert a == b ## assert means that it needs to be true to pass, otherwise fails
+
+    c = Currency.new(amount: 34, code: 'EUR')
+    refute a == c ## refute is opposite of assert
   end
 end
