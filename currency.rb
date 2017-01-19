d8d9bdf8ac8ef622b8c82c75b04152b0f055cc52
cshedlick <carly@automatedinsights.com>
2017-01-19 10:19:22 -0500

    Accepts arguments with currency symbols embedded

--- currency.rb
+++ currency.rb
@@ -1,7 +1,29 @@
+def is_string? string
+ false if Float(string) rescue true
+end
+
 class Currency
   attr_accessor :amount, :code
-  def initialize params = {}
-    params.each { |key, value| send "#{key}=", value }
+  def initialize(options = {})
+
+    amount = options[:amount] || 0
+    code = options[:code] || ''
+
+    @amount = amount.to_s.gsub(/\p{Sc}/,'').to_f.round(2)
+    if code == ''
+      if amount.include? "$"
+        code = 'USD'
+      elsif amount.include? "£"
+        code = 'GBP'
+      elsif amount.include? "€"
+        code = 'EUR'
+      elsif amount.include? "¥"
+        code = 'JPY'
+      elsif amount.include? "₹"
+        code = 'INR'
+      end
+    end
+    @code = code
   end
 
   def == (other)
@@ -9,7 +31,7 @@ class Currency
   end
 
   def +(other)
-    if @code == other.code
+    if code == other.code
       @amount + other.amount
     else
       "DifferentCurrencyCodeError"
--- currency_test.rb
+++ currency_test.rb
@@ -30,4 +30,26 @@ class CurrencyTest < Minitest::Test
     assert a+c == "DifferentCurrencyCodeError"
     assert a-c == "DifferentCurrencyCodeError"
   end
+
+  def test_currency_creation_with_symbols
+    a = Currency.new amount: '$2.00'
+    assert a.amount == 2
+    assert a.code == 'USD'
+
+    b = Currency.new amount: '€2.22'
+    assert b.amount == 2.22
+    assert b.code == 'EUR'
+
+    c = Currency.new amount: '£25'
+    assert c.amount == 25
+    assert c.code == 'GBP'
+
+    d = Currency.new amount: '₹100'
+    assert d.amount == 100
+    assert d.code == "INR"
+
+    e = Currency.new amount: '29.01¥'
+    assert e.amount == 29.01
+    assert e.code == 'JPY'
+  end
 end
